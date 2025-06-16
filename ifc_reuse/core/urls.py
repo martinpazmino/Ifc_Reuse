e10660f8cec84a2d11ce6502275477e57ac1acbe
martinpazmino <martinsebastianp05@gmail.com>
2025-06-17 00:40:35 +0200

    small changes

--- ifc_reuse/core/urls.py
+++ ifc_reuse/core/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('list-ifcs/', views.list_uploaded_ifcs, name='list_ifcs'),
     path('ifc-files/', views.ifc_files_api, name='ifc_files_api'),
     path('upload-fragment/', views.upload_fragment, name='upload_fragment'),
-    path('mark-component/', views.mark_component, name='mark_component'),
+
     path('reusable-components/', views.reusable_components, name='reusable_components'),
     path('api/catalog/', views.catalog_api, name='catalog_api'),
     path('get-element-info/', views.get_element_info_view, name='get_element_info'),
-    path('upload-ifc/', views.upload_ifc_file, name='upload_ifc'),
+
 ]
